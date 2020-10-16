08d3f0d1331b65f77207d6a45e9fac91ec6f5df0
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-16 14:17:28 +0530

    [Shreya]Added: Read and write employeepayroll to console

---
+++ main/java/com/capgemini/EmployeePayrollData.java
@@ -0,0 +1,18 @@
+package com.capgemini;
+
+public class EmployeePayrollData {
+		public int id;
+		public String name;
+		public double salary;
+
+		public EmployeePayrollData(int id, String name, double salary) {
+			this.id = id;
+			this.name = name;
+			this.salary = salary;
+		}
+
+		@Override
+		public String toString() {
+			return "ID = " + id + ", Name = " + name + ", Salary = " + salary;
+		}
+}
---
+++ main/java/com/capgemini/EmployeePayrollService.java
@@ -0,0 +1,40 @@
+package com.capgemini;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class EmployeePayrollService {
+	public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+	private List<EmployeePayrollData> employeePayrollList;
+	
+	public EmployeePayrollService() {
+		
+	}
+	
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList=employeePayrollList;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee ID:");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee Name:");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id,name,salary));
+	}
+	
+	private void writeEmployeePayrollData() {
+		System.out.println("Writing Employee Payroll Roaster to console " + employeePayrollList);
+	}
+
+}
